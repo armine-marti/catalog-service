f0ab1aa7503220835301e9ad48f8bc82a0b7f3f9
Armine Martirosyan <armine.marti@hotmail.com>
2025-03-26 19:51:48 +0000

    Add greeting

---
+++ src/main/java/org/example/catalogservice/HelloController.java
@@ -0,0 +1,13 @@
+package org.example.catalogservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String getGreeting() {
+        return "Welcome to the bookshop catalog";
+    }
+}
